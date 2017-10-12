1a602c92fb320927be719e379a95042336b2b592
ericpcho <ericpcho@gmail.com>
2017-10-12 14:00:42 -0700

    updated test for creating a newGame with a randomNumber

--- src/components/top-nav.test.js
+++ src/components/top-nav.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {TopNav}  from './top-nav';
 import {shallow, mount} from 'enzyme'; 
-import {newGame, toggleInfoModal} from '../actions'
+import {newGame, NEW_GAME, toggleInfoModal} from '../actions'
 
 describe('InfoModal', () => {
     it('should render something', () => {
@@ -26,13 +26,13 @@ describe('InfoModal', () => {
         expect(spy).toHaveBeenCalledWith(toggleInfoModal())
     })
 
-    it.skip('call the newGame method when the + New Game text is clicked', () => {
+    it('call the newGame method when the + New Game text is clicked', () => {
         const spy = jest.fn()
         const wrapper = mount(<TopNav dispatch={spy}/>)
         const button = wrapper.find('.new');
         button.simulate('click')
-        expect(spy).toHaveBeenCalledWith(toggleInfoModal())
+        expect(spy).toHaveBeenCalled()
+        expect(spy.mock.calls[0][0].type).toEqual(NEW_GAME)
+        console.log(spy.mock.calls)        
     })
-
-    
 })
